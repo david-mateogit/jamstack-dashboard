92c842929c3ac3ce02bc811cf7003b2ecf08a0cb
david-mateogit <davidmateo@outlook.com>
2019-12-13 10:57:00 -0400

    private routes

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { useIdentityContext } from 'react-netlify-identity';
 
-const Profile = () => (
-  <div className="dashboard-header">
-    <nav>
-      <Link to="/dashboard/secret" activeClassName="active">
-        Secret Stuff
-      </Link>
-      <Link to="/dashboard/base" activeClassName="active">
-        See Your Base
-      </Link>
-    </nav>
-    <span>TODO Show login status</span>
-  </div>
-);
+const Profile = ({ showModal }) => {
+  const identity = useIdentityContext();
+  const isLoggedIn = identity && identity.isLoggedIn;
+
+  const name =
+    identity &&
+    identity.user &&
+    identity.user.user_metadata &&
+    identity.user.user_metadata.full_name;
+
+  return (
+    isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link to="/dashboard/secret" activeClassName="active">
+            Secret Stuff
+          </Link>
+          <Link to="/dashboard/base" activeClassName="active">
+            See Your Base
+          </Link>
+        </nav>
+        <span>
+          {' '}
+          Logged in as {name}.
+          <button type="button" onClick={showModal}>
+            log out
+          </button>
+        </span>
+      </div>
+    )
+  );
+};
 
 export default Profile;
